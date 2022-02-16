e38f35091f597d595eb97b7a092ede262de50eb4
Seb Ruiz <seb@sebruiz.net>
2022-02-16 10:58:57 +0000

    Add diagnostics

--- homeassistant/components/bhyve/util.py
+++ homeassistant/components/bhyve/util.py
@@ -5,18 +5,10 @@ from .const import CONF_DEVICES
 
 
 def orbit_time_to_local_time(timestamp: str):
-    """Converts the Orbit API timestamp to local time"""
+    """Converts the Orbit API timestamp to local time."""
     if timestamp is not None:
         return dt.as_local(dt.parse_datetime(timestamp))
     return None
-
-
-def anonymize(device):
-    """Removes PII from Orbit API device response"""
-    device["address"] = "REDACTED"
-    device["full_location"] = "REDACTED"
-    device["location"] = "REDACTED"
-    return device
 
 
 def filter_configured_devices(entry: ConfigEntry, all_devices):
